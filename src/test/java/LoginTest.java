64f1c77a67edd1f271bf6da5e5f335a45dcccfe5
katziio <vignesh000129@gmail.com>
2024-02-27 16:39:38 +0530

    terms and privacy

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -17,17 +17,20 @@ import java.time.Duration;
 import java.util.Base64;
 import java.util.EventListener;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class LoginTest {
     public static final String MOBILE_NUMBER = "5111111111";
     public static final String VALID_OTP = "1234";
     public static final String INVALID_OTP = "1111";
     public static final String INVALID_OTP_MESSAGE = "OTP does not match. Please enter the correct OTP.";
-
     public static final int OTP_MAX_LIMIT = 6;
-
     public static final String OTP_MAX_LIMIT_MESSAGE = "You have already made maximum number of attempt to verify OTP. Please try again after some time.";
+    public static final int PRIVACY_X_COORDINATE =  811;
+    public static final int PRIVACY_Y_COORDINATE = 1999;
+    public static final int TERMS_X_COORDINATE = 516;
+    public static final int TERMS_Y_COORDINATE =  2049;
+    public static final int OTP_MAX_SENT_COUNT = 6;
     private AndroidDriver driver;
     private WebDriverWait wait;
 
@@ -105,6 +108,7 @@ public class LoginTest {
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            fail();
         }
         Thread.sleep(3000);
     }
@@ -155,6 +159,7 @@ public class LoginTest {
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            fail();
         }
         Thread.sleep(3000);
     }
@@ -203,6 +208,7 @@ public class LoginTest {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            fail();
         }
     }
 
@@ -261,8 +267,92 @@ public class LoginTest {
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            fail();
         }
         Thread.sleep(3000);
     }
 
+    @Test
+    public void resentOtp() throws InterruptedException {
+        TouchAction touchAction = new TouchAction(driver);
+        Assert.assertNotEquals(touchAction, null);
+
+        int xCoordinate = 355;
+        int yCoordinate = 565;
+        touchAction.tap(PointOption.point(xCoordinate, yCoordinate)).perform();
+        Thread.sleep(5000);
+        WebElement mobileInput = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/et_mobile"));
+        Assert.assertNotEquals(mobileInput, null);
+        System.out.println("Mobile input text: " + MOBILE_NUMBER);
+        mobileInput.click();
+        Thread.sleep(2000);
+        mobileInput.sendKeys(MOBILE_NUMBER);
+        Thread.sleep(1000);
+
+        WebElement proceedBtn = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/proceed"));
+        Assert.assertNotEquals(proceedBtn, null);
+        System.out.println("Proceed button text: " + proceedBtn.getText());
+        proceedBtn.click();
+        Thread.sleep(5000);
+        try {
+            WebElement otpContainer = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/cl_otp_container"));
+            Assert.assertNotEquals(otpContainer, null);
+            WebElement otpLabel = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_otp_label"));
+            WebElement mobileNumberLabel = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_mobile"));
+            WebElement mobileNumberEditCta = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_mobile_ed"));
+            WebElement otpEnterInput = driver.findElement(AppiumBy.className("android.widget.EditText"));
+            String otpLabelText = "Enter OTP sent via SMS";
+            String mobileNumberLabelText = "+91-" + MOBILE_NUMBER;
+            String mobileNumberEditCtaText = "Edit";
+            assertTrue(otpLabelText.trim().equalsIgnoreCase(otpLabel.getText().trim()));
+            assertTrue(mobileNumberLabelText.trim().equalsIgnoreCase(mobileNumberLabel.getText().trim()));
+            assertTrue(mobileNumberEditCtaText.trim().equalsIgnoreCase(mobileNumberEditCta.getText().trim()));
+            Thread.sleep(30000);
+            WebElement resendOtp = null;
+            for (int i = 1; i<=OTP_MAX_SENT_COUNT ; i++) {
+                resendOtp = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_otp_timer"));
+                resendOtp.click();
+                System.out.println("Resend otp click times -> " +i);
+                if(i == (OTP_MAX_SENT_COUNT))
+                {
+                    try {
+                        assertNotEquals(null,driver.findElement(AppiumBy.id("com.app.smytten.debug:id/title_template")));
+                    }catch (Exception e){
+                        fail("toast message not came");
+                    }
+                }
+                Thread.sleep(30000);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            fail();
+        }
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void openTermsAndPolicy() throws InterruptedException{
+        TouchAction touchAction = new TouchAction(driver);
+        Assert.assertNotEquals(touchAction, null);
+
+        int xCoordinate = 355;
+        int yCoordinate = 565;
+        touchAction.tap(PointOption.point(xCoordinate, yCoordinate)).perform();
+        Thread.sleep(5000);
+        touchAction.tap(PointOption.point(PRIVACY_X_COORDINATE, PRIVACY_Y_COORDINATE)).perform();
+        WebElement privacyTitle = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_title"));
+        Thread.sleep(2000);
+        assertEquals("Smytten's Privacy Policy", privacyTitle.getText());
+        Thread.sleep(2000);
+        touchAction.tap(PointOption.point(544, 211)).perform();
+        Thread.sleep(2000);
+        touchAction.tap(PointOption.point(TERMS_X_COORDINATE, TERMS_Y_COORDINATE)).perform();
+        WebElement termsTitle = driver.findElement(AppiumBy.id("com.app.smytten.debug:id/tv_title"));
+        Thread.sleep(2000);
+        assertEquals("Terms of Service", termsTitle.getText());
+        Thread.sleep(2000);
+        touchAction.tap(PointOption.point(544, 211)).perform();
+        Thread.sleep(2000);
+    }
 }
